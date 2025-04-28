540476090b94066f23a9467023c596d7657489eb
Luiz Fernando Morais Alves <lfma020101@gmail.com>
2025-04-28 08:02:50 -0300

    novo bd

--- pizza (4).sql
+++ pizza (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26/04/2025 às 22:51
+-- Tempo de geração: 28/04/2025 às 13:02
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
